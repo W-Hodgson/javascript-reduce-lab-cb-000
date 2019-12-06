ae5cba8302a4e765136928fd0764c75fd3615796
Will Hodgson <will.hodgson@current-rms.com>
2019-12-06 10:05:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,11 +21,13 @@ const totalBatteries = batteryBatches.reduce((total, batch) => total + batch, 0)
 
 
 const wordCountMap = monologueLines.reduce((map, sentence) => {
-  const wordCount = sentence.split(' ').length
+  const wordCount = sentence.split(' ').length;
 
   if (!map[wordCount]) {
     map[wordCount] = 0;
   }
 
   map[wordCount]++;
+
+  return map;
 }, {});
